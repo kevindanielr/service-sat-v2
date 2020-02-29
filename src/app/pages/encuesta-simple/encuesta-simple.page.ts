870dbb5b5b3a9f202d619c94c3ee4981386b8c5e
kevindanielr <kevindanielr@gmail.com>
2020-02-29 11:44:50 -0600

    finalizacion maquetado

--- src/app/pages/encuesta-simple/encuesta-simple.page.ts
+++ src/app/pages/encuesta-simple/encuesta-simple.page.ts
@@ -9,6 +9,53 @@ export class EncuestaSimplePage implements OnInit {
 
   constructor() { }
 
+  //data de prueba
+  opciones = [
+    {
+      opcion: 'Muy malo',
+      imagen: 'https://image.flaticon.com/icons/svg/2307/2307728.svg'
+    },
+    {
+      opcion: 'Malo',
+      imagen: 'https://image.flaticon.com/icons/svg/2307/2307768.svg'
+    },
+    {
+      opcion: 'Regular',
+      imagen: 'https://image.flaticon.com/icons/svg/2307/2307765.svg'
+    },
+    {
+      opcion: 'Bueno',
+      imagen: 'https://image.flaticon.com/icons/svg/2307/2307756.svg'
+    },
+    {
+      opcion: 'Excelente',
+      imagen: 'https://image.flaticon.com/icons/svg/2307/2307753.svg'
+    }
+  ];
+
+  opciones2 = [
+    {
+      opcion: 'Piña',
+      imagen: 'https://image.flaticon.com/icons/svg/1571/1571240.svg'
+    },
+    {
+      opcion: 'Uva',
+      imagen: 'https://image.flaticon.com/icons/svg/135/135542.svg'
+    },
+    {
+      opcion: 'Naranja',
+      imagen: 'https://image.flaticon.com/icons/svg/135/135620.svg'
+    },
+    {
+      opcion: 'Manzana',
+      imagen: 'https://image.flaticon.com/icons/svg/415/415733.svg'
+    },
+    {
+      opcion: 'Banana',
+      imagen: 'https://image.flaticon.com/icons/svg/1135/1135549.svg'
+    }
+  ];
+
   ngOnInit() {
   }
 
